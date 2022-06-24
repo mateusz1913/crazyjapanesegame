f8c5f5a136ffe3b45b938d39564df37a6a2b29d0
Mateusz Mędrek <mateusz.medrek@netguru.com>
2022-06-24 15:47:15 +0200

    feat: frame processor impl (#2)

--- src/PoseFrameProcessor/index.ts
+++ src/PoseFrameProcessor/index.ts
@@ -1,6 +1,30 @@
 import type { Frame } from 'react-native-vision-camera';
 
-export function detectPose(frame: Frame): string[] {
+export type Pose = {
+  leftAnkle: Position;
+  rightAnkle: Position;
+  leftKnee: Position;
+  rightKnee: Position;
+  leftHip: Position;
+  rightHip: Position;
+  leftWrist: Position;
+  rightWrist: Position;
+  leftElbow: Position;
+  rightElbow: Position;
+  leftShoulder: Position;
+  rightShoulder: Position;
+  leftEye: Position;
+  rightEye: Position;
+};
+
+export type Position = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+export function detectPose(frame: Frame): Pose[] {
   'worklet';
+  // @ts-ignore
   return __detectPose(frame);
 }
